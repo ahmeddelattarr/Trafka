44278654dffc06b1fff3aaa454dd60eab40a0750
Ahmed elattar <ahmeddelattarr@outlook.com>
2025-09-04 19:29:56 +0300

    [any message]

--- app/main.py
+++ app/main.py
@@ -1,4 +1,4 @@
-import socket  # noqa: F401
+import socket , struct   # noqa: F401
 
 
 def main():
@@ -9,8 +9,18 @@ def main():
     # Uncomment this to pass the first stage
     #
     server = socket.create_server(("localhost", 9092), reuse_port=True)
-    server.accept() # wait for client
+    conn,addr =server.accept() # wait for client
 
+    print(f"Client connected from {addr}")
+
+    # kafka respone
+    response = struct.pack(">i", 4, 7)
+
+    conn.sendall(response)
+
+    conn.close()
+
+    print("Response sent and connection closed.")
 
 if __name__ == "__main__":
     main()
